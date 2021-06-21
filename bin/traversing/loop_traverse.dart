c7e7ea2a05ec50f921d29c69992f6bff4d008d5e
lazycoder1996 <70382598+lazycoder1996@users.noreply.github.com>
2021-06-21 18:10:49 +0000

    Create loop_traverse.dart

---
+++ bin/traversing/loop_traverse.dart
@@ -0,0 +1,8 @@
+import 'dart:math' as math;
+
+import '../function.dart';
+
+void loop() {
+  var traverseData = readFile('C:/Users/muj/Desktop/MsFiles/loop_traverse.csv');
+  print(traverseData);
+}
