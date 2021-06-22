52d1e43bf8bf0f8f33bdb36b5515b584d8ccfdd3
lazycoder1996 <70382598+lazycoder1996@users.noreply.github.com>
2021-06-22 15:31:27 +0000

    Update loop_traverse.dart

--- bin/traversing/loop_traverse.dart
+++ bin/traversing/loop_traverse.dart
@@ -1,8 +1,23 @@
-import 'dart:math' as math;
-
 import '../function.dart';
 
-void loop() {
+void loopTraverse() {
   var traverseData = readFile('C:/Users/muj/Desktop/MsFiles/loop_traverse.csv');
+  var dataSize = traverseData.length;
+  var backsightData = [];
+  var foresightData = [];
+  var stationData = [];
+  var circleReadings = [];
+  var distanceData = [];
+  var controls = [];
+  var headers = traverseData.sublist(0);
+  for (var i = 1; i < dataSize; i++) {
+    backsightData.add(headers[0]);
+    foresightData.add(headers[2]);
+    stationData.add(headers[1]);
+    circleReadings.add(headers[4]);
+    distanceData.add(headers[5]);
+    controls.add(headers[6]);
+  }
+  stationData.removeWhere((item) => item == '');
   print(traverseData);
 }
