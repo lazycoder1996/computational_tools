c3a54b23f24166abb42757ddb296a1ae45edb844
lazycoder1996 <70382598+lazycoder1996@users.noreply.github.com>
2021-07-08 22:18:40 +0000

    Update loop_traverse.dart

--- bin/traversing/loop_traverse.dart
+++ bin/traversing/loop_traverse.dart
@@ -17,21 +17,103 @@ void loopTraverse() {
     distanceData.add(traverseData[i][5]);
     controls.add(traverseData[i][6]);
   }
-  stationData.removeWhere((item) => item == '');
+  print('backsight data $backsightData');
+  print('foresight data $foresightData');
+  stationData.removeWhere((element) => element == '');
+  print('station data is $stationData');
+  stationData.insert(0, stationData.last);
+  stationData.add(stationData[1]);
   // initialBearing
-  controls.where((element) => element == '');
-  var backSightControls = controls[0].toString().split('//');
-  var stationControls = controls[1].toString().split('//');
+  controls.removeWhere((element) => element == '');
+  var backSightControls = controls[0].toString().split(',');
+  var stationControls = controls[1].toString().split(',');
   var controlData = forwardGeodetic(
       double.parse(backSightControls[0]),
       double.parse(backSightControls[1]),
       double.parse(stationControls[0]),
       double.parse(stationControls[1]));
-
+  // adding distance of controls to distanceData
+  distanceData.insert(0, controlData[0]);
+  distanceData.add(controlData[0]);
+  distanceData.removeWhere((element) => element == '');
+  // print('distance data are $distanceData');
   // computing included angles
   var includedAngles = computeIncludedAngles(circleReadings: circleReadings);
+  num sum = 0;
+  includedAngles.forEach((element) {
+    sum += element;
+  });
+  // print(includedAngles);
+  // print(sum);
   // compute bearings
   var unadjustedBearings = computeBearings(
       includedAngles: includedAngles, initialBearing: controlData[1]);
-  print(controls);
+  // print('initial bearing is ${controlData[1]}');
+  // print('initial bearings are $unadjustedBearings');
+  // adjust bearings
+  var adjustedBearings = adjustBearings(initialBearings: unadjustedBearings);
+  // print('adjusted bearings are $adjustedBearings');
+  // compute dep and lat
+  var depLat =
+      computeDepLat(bearings: adjustedBearings[1], distances: distanceData);
+  // print('dep and lat are $depLat');
+  // adjust dep lat
+  var adjustedDepLat = adjustDepLat(
+    adjustmentMethod: 'Bowditch',
+    distances: distanceData,
+    initialDepLat: depLat,
+  );
+  // print('adjusted dep lat are $adjustedDepLat');
+  // computing eastings and northings
+  var finalCoordinates = computeEastingNorthing(
+      controls: [num.parse(stationControls[0]), num.parse(stationControls[1])],
+      depLat: adjustedDepLat);
+  // print('final coordinates are $finalCoordinates');
+  // finalCoordinates.forEach((element) {
+  //   print(element.toString());
+  // });
+  List<List<dynamic>> output = [];
+  output.insert(0, [
+    'From',
+    'To',
+    'Included Angle',
+    'Distance',
+    'Bearing',
+    'Corr in Bear',
+    'Adjusted Bear',
+    'Departure',
+    'Corr in Dep',
+    'Adjusted Dep',
+    'Latitude',
+    'Corr in Lat',
+    'Adjusted Lat',
+    'Easting',
+    'Northing'
+  ]);
+  includedAngles.insert(0, '');
+  var n = 0;
+  try {
+    while (n <= finalCoordinates[0].length) {
+      output.insert(n + 1, [
+        stationData[n],
+        stationData[n + 1],
+        includedAngles[n],
+        distanceData[n],
+        unadjustedBearings[n],
+        adjustedBearings[0][n],
+        adjustedBearings[1][n],
+        depLat[0][n],
+        adjustedDepLat[0][n],
+        adjustedDepLat[1][n],
+        depLat[1][n],
+        adjustedDepLat[2][n],
+        adjustedDepLat[3][n],
+        finalCoordinates[0][n],
+        finalCoordinates[1][n]
+      ]);
+      n++;
+    }
+  } catch (e) {}
+  downloadResult(
+      'C:/Users/muj/Desktop/MsFiles/loop_traverse_result.csv', output);
 }
